c86862046af6ace5bd0ec399475ae5a0ed3f53bf
Abdelwahab EL Filali <abdelwahab.elfilali@edu.uiz.ac.ma>
2021-04-02 15:37:48 +0100

    Spring boot Project Init

---
+++ src/test/java/ma/GymPro/GymProApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.GymPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
